6c4d5c5fb2ffadef6bc16f49a591911449d99cc4
taifhoon <taifhoon432@gmail.com>
2022-12-17 14:44:14 +0700

    command

---
+++ backend/customer-service/src/main/java/com/example/customerservice/CreateCustomerCommand.java
@@ -0,0 +1,11 @@
+package com.example.customerservice;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Builder
+@Data
+public class CreateCustomerCommand {
+}
